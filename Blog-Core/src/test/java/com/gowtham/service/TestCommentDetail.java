d9311acacb7257e49a415f8be0d4b96b04b2d9bf
GowthamAnbu <gautamgow@gmail.com>
2017-02-14 11:38:13 +0530

    useless object removed

--- Blog-Core/src/test/java/com/gowtham/service/TestCommentDetail.java
+++ Blog-Core/src/test/java/com/gowtham/service/TestCommentDetail.java
@@ -5,7 +5,6 @@ import com.gowtham.dao.CommentDetailDAO;
 public class TestCommentDetail {
 	
 public static void main(String[] args) {
-	CommentDetailService commentDetailService = new CommentDetailService();
 	CommentDetailDAO commentDetailDAO = new CommentDetailDAO();
 	System.out.println(commentDetailDAO.getComments(25, 12));
 //	System.out.println(commentDetailService.getComments(25, 12));
